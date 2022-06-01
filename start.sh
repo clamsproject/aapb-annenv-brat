d81cba4b2b7f87273eb56687a08a56cdd3257a85
Keigh Rim <keigh.rim@gmail.com>
2022-06-01 11:01:31 -0400

    configurations now can be grouped into individual projects

--- start.sh
+++ start.sh
@@ -1,26 +1,29 @@
-if [ -z $1 ]; then 
-    echo "Specify a directory with raw text files" 
+if [ $# -ne 2 ]; then 
+    echo "Specify a project name and a directory with raw text files" 
     exit 1
 fi
-if [ ! -d $1 ]; then 
-    echo "Specify a directory with raw text files" 
+conf_dir="$(dirname $0)/configs/$(basename $1)"
+data_dir="$2"
+if [ ! -d $conf_dif ]; then 
+    echo "Cannot find a configuratino for the project [ $1 ]."
     exit 1
 fi
 
-for f in $1/*; do 
+for f in $data_dir/*.txt; do 
     if [ ! -e ${f%.txt}.ann ] ; then 
         touch ${f%.txt}.ann
     fi
 done
 
-cp ann_conf/* $1/
+set -x
+cp "$conf_dir"/*.conf "$data_dir"
 
 docker run \
     -it \
     --rm \
     --name brat \
     -p 8080:80 \
-    -v $1:/bratdata \
+    -v $data_dir:/bratdata \
     -e BRAT_USERNAME=gbh \
     -e BRAT_PASSWORD=clams \
     -e BRAT_EMAIL=brat@example.com \
